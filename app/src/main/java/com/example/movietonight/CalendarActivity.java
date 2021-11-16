2c92d18d7163b2ef202fd44098b67a7af72de5ca
JPep26 <94396777+JPep26@users.noreply.github.com>
2021-11-16 18:17:43 +0900

    ProfileActivity

--- app/src/main/java/com/example/movietonight/CalendarActivity.java
+++ app/src/main/java/com/example/movietonight/CalendarActivity.java
@@ -1,4 +1,32 @@
 package com.example.movietonight;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class CalendarActivity {
+
+public class CalendarActivity extends AppCompatActivity{
+    public String readDay = null;
+    public String str = null;
+    public CalendarView calendarView;
+    public TextView diaryTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        calendarView = findViewById(R.id.calendarView);
+        diaryTextView = findViewById(R.id.diaryTextView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                diaryTextView.setVisibility(View.VISIBLE);
+                diaryTextView.setText(String.format("%d / %d / %d", year, month + 1, dayOfMonth));
+               \
+            }
+        });
+    }
 }
