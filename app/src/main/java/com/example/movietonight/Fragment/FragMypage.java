cc2c96e307ae4b4038f67e206e2872530cb69870
Seon <sunju701@gmail.com>
2021-11-16 18:41:05 +0900

    mypage

--- app/src/main/java/com/example/movietonight/Fragment/FragMypage.java
+++ app/src/main/java/com/example/movietonight/Fragment/FragMypage.java
@@ -1,25 +1,61 @@
 package com.example.movietonight.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.example.movietonight.CalendarActivity;
+import com.example.movietonight.LogoutActivity;
+import com.example.movietonight.MyReviewActivity;
 import com.example.movietonight.R;
+import com.example.movietonight.RankingActivity;
+import com.example.movietonight.SavedActivity;
 
 public class FragMypage extends Fragment {
-
+    private Button review, saved, cal, rank, logout;
     private View view;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.frag_mypage, container, false);
-
+        review = view.findViewById(R.id.btn_review);
+        saved = view.findViewById(R.id.btn_saved);
+        cal = view.findViewById(R.id.btn_calendar);
+        rank = view.findViewById(R.id.btn_ranking);
+        logout = view.findViewById(R.id.btn_logout);
         return view;
+    }
+    public void onClick(View v){
+        Intent intent;
+        switch(v.getId()){
+            case R.id.btn_review:
+                intent = new Intent(getActivity(), MyReviewActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_saved:
+                intent = new Intent(getActivity(), SavedActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_calendar:
+                intent = new Intent(getActivity(), CalendarActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_ranking:
+                intent = new Intent(getActivity(), RankingActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.btn_logout:
+                intent = new Intent(getActivity(), LogoutActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/movietonight/SavedActivity.java
+++ app/src/main/java/com/example/movietonight/SavedActivity.java
@@ -1,4 +1,53 @@
 package com.example.movietonight;
 
-public class SavedActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class SavedActivity extends AppCompatActivity {//찜한 리스트를 아이템으로 넣고 화면에 보여주는 클래스
+    RecyclerView recyclerView;
+    FavoriteListAdapter adapter;
+    ImageButton btnBack;
+    ArrayList<FavoriteMovie> favoriteMovies=new ArrayList<FavoriteMovie>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.favorite_list_recycler);
+        recyclerView = (RecyclerView)findViewById(R.id.favoriteMovie_recycler);
+        btnBack=(ImageButton)findViewById(R.id.btnBack);//뒤로가기 버튼
+
+        recyclerView.setLayoutManager(new LinearLayoutManager
+                (this, RecyclerView.VERTICAL, false)) ;
+        adapter = new FavoriteListAdapter();
+
+        favoriteMovies();//db에서 찜목록을 가져옴
+        setFavoriteMovies();//item추가
+        recyclerView.setAdapter(adapter);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //뒤로가기, 인텐트 종료
+                //finish();
+            }
+        });
+    }
+    public void favoriteMovies() {//db에서 찜영화 가져오는 메서드
+        //db에서 찜 영화 가져옴
+        for (int i = 0; i < 10; i++) {
+            String str = i + "번영화";
+            FavoriteMovie item=new FavoriteMovie(str);
+            favoriteMovies.add(item);//아이템을 리스트에 넣기
+        }
+    }
+    public void setFavoriteMovies(){//아이템 추가 메서드
+        for (int i=0;i<favoriteMovies.size();i++){
+            adapter.setFavoriteMovieData(favoriteMovies.get(i));
+        }
+    }
 }
