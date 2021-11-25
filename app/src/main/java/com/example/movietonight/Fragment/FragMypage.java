b77f1f67034182d9f404d343f8b93a783fe14c48
DoYeon <dodoyeon0425@gmail.com>
2021-11-25 19:06:46 +0900

    마이페이지 수정

--- app/src/main/java/com/example/movietonight/Fragment/FragMypage.java
+++ app/src/main/java/com/example/movietonight/Fragment/FragMypage.java
@@ -15,17 +15,28 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.movietonight.CalendarActivity;
+import com.example.movietonight.FollowerActivity;
 import com.example.movietonight.FollowingListActivity;
 import com.example.movietonight.MyReviewActivity;
 import com.example.movietonight.R;
 import com.example.movietonight.RankingActivity;
 import com.example.movietonight.SavedActivity;
 import com.example.movietonight.StartActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class FragMypage extends Fragment implements View.OnClickListener{
     private Button review, saved, cal, rank, logout;
     private View view;
-    private TextView following;
+    private TextView following,follwer,userName;
+    private FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
+    private DatabaseReference databaseReference=firebaseDatabase.getReference("UserAccount");
+    private FirebaseUser user= FirebaseAuth.getInstance().getCurrentUser();
 
     @Nullable
     @Override
@@ -37,12 +48,16 @@ public class FragMypage extends Fragment implements View.OnClickListener{
         rank = view.findViewById(R.id.btn_ranking);
         logout = view.findViewById(R.id.btn_logout);
         following=view.findViewById(R.id.following);
+        follwer=view.findViewById(R.id.follower);
+        userName=view.findViewById(R.id.username);
         review.setOnClickListener(this);
         saved.setOnClickListener(this);
         cal.setOnClickListener(this);
         rank.setOnClickListener(this);
         logout.setOnClickListener(this);
         following.setOnClickListener(this);
+        follwer.setOnClickListener(this);
+        getUserNickname();
         return view;
     }
     public void onClick(View v){
@@ -83,6 +98,24 @@ public class FragMypage extends Fragment implements View.OnClickListener{
                 intent=new Intent(getActivity(), FollowingListActivity.class);
                 startActivity(intent);
                 break;
+            case R.id.follower:
+                intent=new Intent(getActivity(), FollowerActivity.class);
+                startActivity(intent);
+                break;
         }
+    }
+    public void getUserNickname(){
+        databaseReference.child(user.getUid()).child("userNickname").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String nickName= (String) snapshot.getValue();
+                userName.setText(nickName);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
