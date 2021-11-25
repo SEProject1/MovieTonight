d85fea98bf8aafc4d4b2d699fd5b5efad657fb50
JPep26 <94396777+JPep26@users.noreply.github.com>
2021-11-25 18:50:16 +0900

    RankingActivity 살짝수정> DB연결필요

--- app/src/main/java/com/example/movietonight/RankingActivity.java
+++ app/src/main/java/com/example/movietonight/RankingActivity.java
@@ -25,7 +25,6 @@ public class RankingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ranking);
 
         pieChart = (PieChart)findViewById(R.id.piechart);
-
         pieChart.setUsePercentValues(true);
         pieChart.getDescription().setEnabled(false);
         pieChart.setExtraOffsets(5,10,5,5);
