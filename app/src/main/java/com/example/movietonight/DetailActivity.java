d09d47e8c9a8a02c6695b1d25e7e129c60aeebcd
seonghyeokparKK <gottaa58@gmail.com>
2021-11-25 15:02:35 +0900

    장르(미스테리) 추가 및 리뷰 작성시 장르 값 자동 적용

--- app/src/main/java/com/example/movietonight/DetailActivity.java
+++ app/src/main/java/com/example/movietonight/DetailActivity.java
@@ -73,7 +73,7 @@ public class DetailActivity extends AppCompatActivity {
         // 장르
         HashMap<Integer, String> map = new HashMap<>(18);
         map.put(12,"모험 ");map.put(14,"판타지 ");map.put(16,"애니메이션 ");map.put(18,"드라마 ");map.put(27,"공포 ");map.put(28,"액션 ");map.put(35,"코미디 ");map.put(36,"역사 ");map.put(37,"서부 ");
-        map.put(53,"스릴러 ");map.put(80,"범죄 ");map.put(99,"다큐멘터리 ");map.put(878,"SF ");map.put(10402,"음악 ");map.put(10749,"로맨스 ");map.put(10751,"가족 ");map.put(10752,"전쟁 ");map.put(10770,"TV Movie ");
+        map.put(53,"스릴러 ");map.put(80,"범죄 ");map.put(99,"다큐멘터리 ");map.put(9648,"미스테리 ");map.put(878,"SF ");map.put(10402,"음악 ");map.put(10749,"로맨스 ");map.put(10751,"가족 ");map.put(10752,"전쟁 ");map.put(10770,"TV Movie ");
         TextView textView_genre_ids = (TextView)findViewById(R.id.tv_genre_ids);
         // ArrayList의 Integer가 map을 통해 String으로 변환후 모아서 setText
         for(int i=0; i<genre_ids.size(); i++)
