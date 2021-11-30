cd189212cf055df17fb77d49ae2e0ee9396d1097
DoYeon <dodoyeon0425@gmail.com>
2021-11-30 16:10:03 +0900

    feed없을때 없다고 토스트 메시지 언팔로우할때 확인창 띄움

--- app/src/main/java/com/example/movietonight/FollowingListAdapter.java
+++ app/src/main/java/com/example/movietonight/FollowingListAdapter.java
@@ -1,6 +1,9 @@
 package com.example.movietonight;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -59,18 +62,32 @@ public class FollowingListAdapter extends RecyclerView.Adapter<FollowingListView
             @Override
             public void onClick(View view) {
                 //db 팔로잉 리스트에서 해당 유저 제거
-                databaseReference.child(user.getUid()).child("following").child(idToken).
-                        removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                AlertDialog.Builder msgBilder=new AlertDialog.Builder(view.getContext());
+                msgBilder.setTitle("정말로 언팔로우 하시겠습니까?");
+                msgBilder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
                     @Override
-                    public void onSuccess(Void unused) {
-                        //해당 유저의 팔로워 목록에서 사용자 제거
-                        databaseReference.child(idToken).child("follower").child(user.getUid()).removeValue();
-                        Toast.makeText(view.getContext(),"언팔로우 되었습니다.",Toast.LENGTH_LONG).show();
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        databaseReference.child(user.getUid()).child("following").child(idToken).
+                                removeValue().addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                //해당 유저의 팔로워 목록에서 사용자 제거
+                                databaseReference.child(idToken).child("follower").child(user.getUid()).removeValue();
+                                Toast.makeText(view.getContext(),"언팔로우 되었습니다.",Toast.LENGTH_LONG).show();
+                            }
+                        });
+                        myFollowingList.remove(holder.getAdapterPosition());//UI에서 해당 item삭제
+                        notifyItemRemoved(holder.getAdapterPosition());
+                        notifyItemRangeChanged(holder.getAdapterPosition(),myFollowingList.size());
                     }
                 });
-                myFollowingList.remove(holder.getAdapterPosition());//UI에서 해당 item삭제
-                notifyItemRemoved(holder.getAdapterPosition());
-                notifyItemRangeChanged(holder.getAdapterPosition(),myFollowingList.size());
+                msgBilder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(view.getContext(),"취소되었습니다.",Toast.LENGTH_LONG).show();
+                    }
+                });
+                msgBilder.show();
             }
         });
     }
